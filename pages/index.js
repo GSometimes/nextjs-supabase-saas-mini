965a5b195a3a431669e6334f13e1374266f542a9
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-12-06 00:13:43 -0800

    added [id].js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-import { supabase } from "../utils/supabase"
+import { supabase } from "../utils/supabase";
+import Link from "next/link";
 
 export default function Home({ lessons }) {
-  console.log({lessons});
+  console.log({ lessons });
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      {lessons.map(lesson => (
-        <p key={lesson.id}>{lesson.title}</p>
+    <div className="w-full max-w-3xl mx-auto my-16 px-2">
+      {lessons.map((lesson) => (
+        <Link href={`/${lesson.id}`} key={lesson.id}>
+          <a className="p-8 h-40 mb-4 rounded shadow text-xl flex">{lesson.title}</a>
+        </Link>
       ))}
     </div>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
-  const { data: lessons } = await supabase.from('lesson').select('*')
+  const { data: lessons } = await supabase.from("lesson").select("*");
 
   return {
     props: {
       lessons,
-    }
-  }
-}
+    },
+  };
+};
